628697544aa186f78b9196fbc93fec0c71fa66bb
chenshuai <1448509198@qq.com>
2019-11-18 16:10:25 +0800

    增加第三句本地仓库

--- src/com/hs/git/FirstClass.java
+++ src/com/hs/git/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("helloworldtoo");
+        System.out.println("helloworldtootoo");
     }
 
 }
